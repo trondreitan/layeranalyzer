831ab7c9b1d4dcbc24ffa0e45191ffc4ddb5b01d
Trond Reitan <trond.reitan@ibv.uio.no>
2024-10-02 12:37:21 +0200

    Added descriptions to layered and layer.series.structure objects.

---
+++ R/layeranalyzer_loglik.R
@@ -0,0 +1,88 @@
+library(coda)
+
+
+layer.param.loglik=function(analysis, new.param.values=NULL, silent.mode=TRUE, num.optim=100)
+{
+ if(is.null(analysis))
+ {
+   stop("The 'analysis' object must exists (and be a 'layered' object)!")
+ }
+ if(sum(class(analysis)=="layered")==0)
+ {
+   stop("The 'analysis' object must be a 'layered' object!")
+ }
+
+ analysis2=analysis
+ analysis2$input.options$num.MCMC=1
+ if(is.null(new.param.values))
+ {
+  analysis2$mcmc=analysis2$mcmc.origpar=coda::mcmc(matrix(analysis2$est.par,nrow=1))
+ }
+ 
+ if(!is.null(new.param.values))
+ {
+  if(length(analysis2$est.origpar)!=length(new.param.values))
+    stop("Number of new input parameter values has a length that does not match the models number of parameters (as specified in 'est.origpar')!")
+  analysis2$mcmc=analysis2$mcmc.origpar=new.param.values
+ }
+ 
+ # need to transform back to characteristic times if half life
+ numpar=length(analysis2$est.origpar)
+ if(analysis2$input.options$use.half.lives) 
+  for(j in 1:numpar)
+  {
+   if(substr(analysis2$parameter.names[j],1,3)=="dt_")
+     analysis2$mcmc[j,]=analysis2$mcmc[j,]/log(2)
+  }
+ analysis2$mcmc[which(is.na(analysis2$mcmc), arr.ind=TRUE)] = -1e+7
+ analysis2$mcmc.origpar[which(is.na(analysis2$mcmc.origpar), arr.ind=TRUE)] = -1e+7
+ analysis2$mcmc=coda::as.mcmc(rbind(analysis2$mcmc))
+ analysis2$mcmc.origpar=coda::as.mcmc(rbind(analysis2$mcmc.origpar))
+     
+ res=layer.analyzer.timeseries.list(analysis$data.structure,previous.run=analysis2,silent.mode=silent.mode, maximum.likelihood.numstart=num.optim)
+     
+ return(res$loglik)
+}
+
+
+layer.param.logliks=function(analysis, new.param.value.sets, silent.mode=TRUE, num.optim=100)
+{
+ if(is.null(analysis))
+ {
+   stop("The 'analysis' object must exists (and be a 'layered' object)!")
+ }
+ if(sum(class(analysis)=="layered")==0)
+ {
+   stop("The 'analysis' object must be a 'layered' object!")
+ }
+ 
+ analysis2=analysis
+ analysis2$input.options$num.MCMC=1
+ if(is.null(new.param.value.sets))
+ {
+   stop("Parameter sets must be given!")
+ }
+ 
+ if(length(analysis2$est.origpar)!=dim(new.param.value.sets)[2])
+    stop("Number of new input parameter values has a length that does not match the models number of parameters (as specified in 'est.origpar')!")
+  analysis2$mcmc=analysis2$mcmc.origpar=new.param.value.sets
+ 
+ # need to transform back to characteristic times if half life
+ numpar=length(analysis2$est.origpar)
+ if(analysis2$input.options$use.half.lives) 
+  for(j in 1:numpar)
+  {
+   if(substr(analysis2$parameter.names[j],1,3)=="dt_")
+     analysis2$mcmc[j,]=analysis2$mcmc[j,]/log(2)
+  }
+ analysis2$mcmc[which(is.na(analysis2$mcmc), arr.ind=TRUE)] = -1e+7
+ analysis2$mcmc.origpar[which(is.na(analysis2$mcmc.origpar), arr.ind=TRUE)] = -1e+7
+ analysis2$mcmc=coda::as.mcmc(analysis2$mcmc)
+ analysis2$mcmc.origpar=coda::as.mcmc(analysis2$mcmc.origpar)
+     
+ res=layer.analyzer.timeseries.list(analysis$data.structure,previous.run=analysis2,silent.mode=silent.mode, maximum.likelihood.numstart=num.optim)
+     
+ return(res$loglik)
+}
+
+
