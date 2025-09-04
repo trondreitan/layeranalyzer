df28b9d744ece6a2cebcef16f367f5e84919d1a4
adamkocsis <adam.t.kocsis@gmail.com>
2025-09-04 16:38:46 +0200

    Registered Rccp methods
    CRAN check says:
    "It is good practice to register native routines and to disable symbol
    search."

---
+++ src/init.c
@@ -0,0 +1,23 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+
+/* .Call calls */
+extern SEXP layeranalyzer(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+
+static const R_CMethodDef CEntries[] = {
+  {NULL, NULL, 0}
+};
+
+static const R_CallMethodDef CallEntries[] = {
+  {"layeranalyzer",                 (DL_FUNC) &layeranalyzer,                 25},
+  {NULL, NULL, 0}
+};
+
+void R_init_layeranalyzer(DllInfo *dll)
+{
+  R_registerRoutines(dll, CEntries, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
