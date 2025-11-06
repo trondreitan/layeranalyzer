134e477c4c2e903d969d27ce34e5d85f3a25e331
Trond Reitan <trond.reitan@ibv.uio.no>
2025-11-06 15:04:08 +0100

    testing init.c on windows 2.

--- src/init.c
+++ src/init.c
@@ -1,6 +1,6 @@
-//#ifdef __linux__
+#ifdef __linux__
 
-//#include <R.h>
+#include <R.h>
 #include <Rinternals.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
@@ -24,4 +24,4 @@ void R_init_layeranalyzer(DllInfo *dll)
   R_useDynamicSymbols(dll, FALSE);
 }
 
-// #endif // __linux__
+#endif // __linux__
