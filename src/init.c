256fe8bb3fe89f016e5aa148bac98b16dea867e1
Trond Reitan <trondreitan@proton.me>
2026-07-10 11:18:47 +0200

    Add files via upload
    Changed the number of inputs to C++ code (Adam Kocsis and Trond Reitan). Added input parameter names in comments to call on layeranalyzer so it would be easier to track (Trond). Made code available in Windows and Mac again (Adam Kocsis).

--- src/init.c
+++ src/init.c
@@ -1,4 +1,4 @@
-#ifdef __linux__
+/* #ifdef __linux__ // not necessary? */
 
 #include <R.h>
 #include <Rinternals.h>
@@ -7,14 +7,42 @@
 
 
 /* .Call calls */
-extern SEXP layeranalyzer(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP layeranalyzer(SEXP /*input*/,
+			  SEXP /*num_MCMC*/,
+			  SEXP /*Burnin*/,
+			  SEXP /*Spacing*/,
+			  SEXP /*NumTemp*/,
+			  SEXP /*do_model_likelihood*/,
+			  SEXP /*do_maximum_likelihood*/,
+			  SEXP /*maximum_likelihood_numstart*/,
+			  SEXP /*SilentMode*/,
+			  SEXP /*TalkativeBurnin*/,
+			  SEXP /*TalkativeLikelihood*/,
+			  SEXP /*IdStrategy*/,
+			  SEXP /*UseStationarySdev*/,
+			  SEXP /*TempGround*/,
+			  SEXP /*UseHalfLives*/,
+			  SEXP /*ReturnMCMC*/, 
+			  SEXP /*causal*/,
+			  SEXP /*causal_symmetric*/,
+			  SEXP /*corr*/,
+			  SEXP /*smooth_specs*/,
+			  SEXP /*realization_specs*/,
+			  SEXP /*ReturnResiduals*/,
+			  SEXP /*mode*/,
+			  SEXP /*loglik_laxness*/,
+			  SEXP /*input_param_values*/,
+			  SEXP /*input_mcmc*/,
+			  SEXP /*external_input*/,
+			  SEXP /*distance_matrix*/);
+
 
 static const R_CMethodDef CEntries[] = {
   {NULL, NULL, 0}
 };
 
 static const R_CallMethodDef CallEntries[] = {
-  {"layeranalyzer",                 (DL_FUNC) &layeranalyzer,                 26},
+  {"layeranalyzer",                 (DL_FUNC) &layeranalyzer,28},
   {NULL, NULL, 0}
 };
 
@@ -24,4 +52,4 @@ void R_init_layeranalyzer(DllInfo *dll)
   R_useDynamicSymbols(dll, FALSE);
 }
 
-#endif // __linux__
+// #endif // __linux__
